c6bd718a81eb15cc6a569dccc5620e56e15a8fff
antonijek <antonijek@yahoo.com>
2021-05-20 21:38:43 +0200

    checkboxovi i filteri

--- src/components/ProductsList.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ProductsList = () => {
-  return(
-    <div className="flex flex-grow items-center justify-center">Lista proizvoda</div>
-  );
-}
-
-export default ProductsList;
