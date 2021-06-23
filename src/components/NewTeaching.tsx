d362681139199d80e83a3b9d35e0867878b936fa
Nikola <zelezekas@gmail.com>
2021-06-23 19:50:07 +0200

    NewTeaching component updated

--- src/components/NewTeaching.tsx
+++ src/components/NewTeaching.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 const NewTeaching = () => {
-  return(
-    <div className="flex flex-grow items-center justify-center">Nova pouka</div>
-  );
-}
+	return (
+		<form>
+			<div className="flex justify-evenly mt-1/12 mb-1/12">
+				<div className="flex-col">
+					<p className="text-gray-200">Unesite naslov pouke</p>
+					<TextField label="naslov" />
+				</div>
+				<div className="flex-col">
+					<p className="text-gray-200">Unesite redni broj pouke</p>
+					<TextField label="broj" />
+				</div>
+			</div>
+			<div className="flex justify-evenly">
+				<div className="mx-auto">
+					<p className="text-gray-200">Unesite pouku</p>
+					<TextField label="pouka" variant="filled" multiline rowsMax={5} />
+				</div>
+			</div>
+		</form>
+		// </div>
+	);
+};
 
 export default NewTeaching;
